b08f6cacd0f5a8a5c4f6b5f01988a6e5bc56a2f1
Pravv13 <gpravasta13@gmail.com>
2024-09-02 19:01:58 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,7 @@
+# Hitung luas lingkaran sederhana
+def luas_lingkaran():
+    r = int(input("Masukkan panjang jari-jari lingkaran: "))
+    luas = 3.14*r*r
+    print("Luas lingkaran adalah: ", luas)
+
+luas_lingkaran()
