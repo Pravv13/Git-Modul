29b1f84367e3e0c943487b626c44050750d37303
Pravv13 <gpravasta13@gmail.com>
2024-09-02 20:08:26 +0700

    Menambahkan fungsi hitung volume tabung

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 # Hitung luas lingkaran sederhana
 def luas_lingkaran():
+    print("-----Program hitung luas lingkaran-----")
     r = int(input("Masukkan panjang jari-jari lingkaran: "))
     luas = 3.14*r*r
     print("Luas lingkaran adalah: ", luas)
 
 luas_lingkaran()
+print("<<<<<<>>>>>")
+# Hitung volume tabung sederhana
+def vol_tabung():
+    print("-----Program hitung volume tabung-----")
+    r = int(input("Masukkan panjang jari-jari lingkaran: "))
+    t = int(input("Masukkan tinggi tabung: "))
+    vol = 3.14*r*r*t
+    print("Volume tabung adalah: ", vol)
+
+vol_tabung()
